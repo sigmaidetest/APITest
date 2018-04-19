db8a6d711a3d0f2d7e3db93dcfc922998868bebf
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-04-19 10:25:05 +0530

    init

---
+++ APITest/lambda.js
@@ -0,0 +1,14 @@
+let AWS = require('aws-sdk');
+exports.handler = function(event, context, callback) {
+
+	// let response = {
+	// 	"isBase64Encoded": 1,
+	// 	"statusCode": 200,
+	// 	"headers": {
+	// 		"h1": "h1v"
+	// 	},
+	// 	"body": {msg: 'Hello there!'}
+	// };
+
+	callback(null, 'Hello there!');
+}
